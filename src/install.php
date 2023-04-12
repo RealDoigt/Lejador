fa46f91101cb1afaab1af5a303c6a39cb8e5f1df
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-04-12 17:32:57 -0400

    move file writes + headback

--- src/install.php
+++ src/install.php
@@ -1,15 +1,18 @@
 <?php
 
-function create_db($server, $username, $password, $connection)
+if (!isset($_POST['username']))
+{
+    header('location: install.htm');
+    exit;
+}
+
+function create_db($server)
 {
     $result = -1;
 
     if ($connection->query('create database lejadorDB;'))
     {
         $settings_file = fopen('settings.csv', 'w');
-
-        fwrite($settings_file, "$server,$username,$password");
-        fclose($settings_file);
         $result = 0;
     }
 
@@ -135,9 +138,14 @@ function create_tables($connection)
 function main()
 {
     $connection = new mysqli('localhost', 'root', '');
+
     if ($connection->connect_error) die($conn->connect_error);
-    if (
-    
+
+    if (create_db($connection) === 0)
+    {
+        fwrite($settings_file, "$server,$username,$password");
+        fclose($settings_file);
+    }
 }
 
 main();
