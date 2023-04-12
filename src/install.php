e66aef9a9816c902b87e19b30f51ad343c00fae1
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-04-12 16:36:58 -0400

    create tables fn

--- src/install.php
+++ src/install.php
@@ -43,6 +43,95 @@ class Table
     }
 }
 
+function create_tables($connection)
+{
+    $tables = array
+    (
+        new Table
+        (
+            array
+            (
+                'user_id int not null auto_increment',
+                'name varchar(20) not null',
+                'is_admin bool not null',
+                'is_moderator bool not null',
+                'is_creator bool not null',
+                'primary key(user_id)'
+            ),
+
+            'Users'
+        ),
+
+        new Table
+        (
+            array
+            (
+                'comic_id int not null auto_increment',
+                'name varchar(50) not null',
+                'description varchar(500) not null',
+                'creator_id int not null',
+                'primary key(comic_id)',
+                'foreign key(creator_id) references Users(user_id)'
+            ),
+
+            'Comics'
+        ),
+
+        new Table
+        (
+            array
+            (
+                'page_id int not null auto_increment',
+                'page_number int not null',
+                'image_path varchar not null',
+                'date_published date not null'
+                'comic_id int not null',
+                'primary key(page_id)',
+                'foreign key(comic_id) references(comic_id)'
+            ),
+
+            'Pages'
+        ),
+
+        new Table
+        (
+            array
+            (
+                'comment_id int not null auto_increment',
+                'content tinytext not null',
+                'date_published date not null',
+                'commenter_id int not null',
+                'page_id int not null',
+                'primary key(comment_id)',
+                'foreign key(comment_id) references Users(user_id)',
+                'foreign key(page_id) references Pages(page_id)'
+            ),
+
+            'Comments'
+        ),
+
+        new Table
+        (
+            array
+            (
+                'feedback_id int not null auto_increment',
+                'review varchar(500)',
+                'date_published date',
+                'stars int not null',
+                'reviewer_id int not null',
+                'comic_id int not null',
+                'primary key(feedback_id)',
+                'foreign key(reviewer_id) references Users(user_id)',
+                'foreign key(comic_id) references Comics(comic_id)'
+            ),
+
+            'Feedbacks'
+        ),
+    );
+
+    for ($tables as $t) $t->create($connection);
+}
+
 function main()
 {
     $connection = new mysqli('localhost', 'root', '');
