f452abef2ffe7812acb161d36cee71444d5c8429
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-04-13 14:51:53 -0400

    Update db.php
    dtor + refactor

--- src/db.php
+++ src/db.php
@@ -1,6 +1,6 @@
 <?php
 
-    class DB
+    abstract class TableHelper
     {
         private $connection;
 
@@ -8,9 +8,17 @@
         {
             $settings_file = fopen('settings.csv', 'r');
             $settings = explode(',', fread($settings_file));
-            $connection = new msqli($settings[0], $settings[1], $settings[2]);
+            $connection = new mysqli($settings[0], $settings[1], $settings[2]);
 
             if ($connection->connect_error) die($conn->connect_error);
+            $connection->select_db('lejadorDB');
+        }
+
+        function __destruct()
+        {
+            $connection->close();
         }
     }
+
+
 ?>
