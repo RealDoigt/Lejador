d56f9ed6e15993df775d1072457115f2ba8921d8
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-04-13 15:35:12 -0400

    select + refactor

--- src/db.php
+++ src/db.php
@@ -4,14 +4,14 @@
     {
         private $connection;
 
-        function __construct(protected $attributes, protected $name)
+        function __construct(protected $attributes, protected $name, private $id_name)
         {
             $settings_file = fopen('settings.csv', 'r');
             $settings = explode(',', fread($settings_file));
-            $connection = new mysqli($settings[0], $settings[1], $settings[2]);
+            $this->connection = new mysqli($settings[0], $settings[1], $settings[2]);
 
-            if ($connection->connect_error) die($connection->connect_error);
-            $connection->select_db('lejadorDB');
+            if ($this->connection->connect_error) die($connection->connect_error);
+            $this->connection->select_db('lejadorDB');
         }
 
         function __destruct()
@@ -21,9 +21,13 @@
 
         protected function insert_into($values)
         {
-            $query = "insert into $name (" . implode(',', $attributes) . 'values (' . implode(',', $values) . ');';
-            if (!$connection->query($query)) echo $query;
+            $query = "insert into $this->name (" . implode(',', $this->attributes) . 'values (' . implode(',', $values) . ');';
+            if (!$this->connection->query($query)) echo $query;
+        }
 
+        procected function get_values($fields = ['*'])
+        {
+            return $this->connection->query('select ' . implode(',', $fields) . " from $this->name;");
         }
     }
 ?>
