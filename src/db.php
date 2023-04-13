38921060453d6297cfd2aa2f51a7b9c62e1a1022
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-04-13 16:10:56 -0400

    salt + create u + u count

--- src/db.php
+++ src/db.php
@@ -3,6 +3,7 @@
     class Table_Helper
     {
         private $connection;
+        private $salt = 'fouehwq8p9637436rfewapqo38284372yrewrhjud';
 
         function __construct(protected $attributes, protected $name)
         {
@@ -29,6 +30,11 @@
         {
             return $this->connection->query('select ' . implode(',', $fields) . " from $this->name;");
         }
+
+        protected function get_salt()
+        {
+            return $salt;
+        }
     }
 
     class User_Helper extends Table_Helper
@@ -36,6 +42,16 @@
         function __construct()
         {
             parent::__construct(['name', 'pass', 'is_admin', 'is_moderator', 'is_creator'], 'Users');
+        }
+
+        function create_user($username, $password)
+        {
+            $this->insert_into([$username, crypt($password, $this->get_salt(), $this->get_user_count() == 0 ? 1 : 0, 0, 0]));
+        }
+
+        private function get_user_count()
+        {
+            return $this->get_values(['count(user_id)']);
         }
     }
 ?>
